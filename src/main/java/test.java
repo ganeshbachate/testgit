b861ec68421775c2a1eb467e429d1e96b8baf527
ganesh <ganeshbachategmb999@gmail.com>
2021-01-17 12:47:07 +0530

    2 dockerfile

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("hey hi hello jenkins hello");
+        
+        System.out.println("hey hi hello jenkins hello dockerfile");
     }
 }
